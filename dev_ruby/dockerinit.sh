12f120bf4625f9cc1da7c78147c496825a460973
Alex Blackie <alex@alexblackie.com>
2016-11-07 13:06:59 -0800

    Only change UID if it's not root

--- dev_ruby/dockerinit.sh
+++ dev_ruby/dockerinit.sh
@@ -2,7 +2,10 @@
 
 # Change the UID based on volume, since we can't assume UID 1000 will match
 # everyone's situation. This is especially not true for enterprise logins.
-usermod -u $(stat -c '%u' /data) developer
+USER_ID=$(stat -c '%u' /data)
+if [ "$USER_ID" -gt "0" ] ; then
+	usermod -u $USER_ID developer
+fi
 
 # Automatically enter the mounted application root
 cd /data
